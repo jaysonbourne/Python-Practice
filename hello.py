840ef8b427af8dd3864079ef49406b17c725c35c
J.Kim <25806247+jsyoungk@users.noreply.github.com>
2018-02-19 15:28:57 -0800

    Strings and Lists
    A simple review and run through of basic algorithms with Python. 
    There are still some mistakes that need to be fixed. But overall all of them should translate very easy.

---
+++ hello.py
@@ -0,0 +1,80 @@
+# print ("hello world.")
+# x = "hello world"
+# print (x)
+# y = 42
+# print (y)
+# def say_hello(name):
+#     # name = "Johanna";
+#     if name:
+#         print ("Hello, " + name + " from inside the python shell.")
+#     else:
+#         print ("No name")
+# print (say_hello("Johanna"))
+#
+# print ("Outisde of the function.")
+# print ("This is a sample string.")
+#
+# # name = "Julie"
+# print("My name is {name}.")
+# name = "Shawen"
+# age = 19
+# if age >= 18:
+#     print ("Legal: Allowed to Buy Alcohol.")
+# else:
+#     print ("Not Allowed.")
+# if age >= 18:
+#     print ("Legal: Allowed")
+# elif age == 15:
+#     print ("Illegal: Not Allowed.")
+# else:
+#     print ("Don't Sell.")
+# for count in range(0, 5):
+#     print ("looping...", count)
+#     # print (count)
+# my_list = [4, "dog", 99, ["list","inside","a list"]]
+# count = 0
+# # print (my_list[1])
+# while count < 5:
+#     print (my_list[3], count)
+#     count += 1
+# for val in "string":
+#     if val == "i":
+#         break
+#     print (val)
+# for val in "string":
+#     if val == "i":
+#         pass
+#     print (val)
+# x = 3
+# y = x
+# while y > 0:
+#     print (y)
+#     y = y -1
+# else:
+#     print ("Final else statement.")
+# print("hello" + "world")
+# print(hello + world)
+# for val in "Today is Thanksgiving day! It's also my birthday":
+#     if val == "d":
+#         break
+#     print (val)
+
+# Find and Replace
+str = "It's Thanksgiving day. It's also my birthday, too!"
+print (str.replace("day", "Month"))
+
+#Min and Max
+x = [1,2,3,4,5,6,7,200,-1]
+print (min(x))
+print (max(x))
+
+#First and Last
+var = ["hello","world"]
+first = (var[0], var[-1])
+print (first)
+
+#New list
+y = [19,2,54,-2,7,12,98,32,10,-3,6]
+y.sort()
+print (y) #this will print out a sorted list now
+ 
